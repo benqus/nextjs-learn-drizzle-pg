7747b9cbba557e7f768146275c6246f6d3dc258e
Bence Kormos <ben.kormos@gmail.com>
2024-03-14 08:44:02 +0100

    adding metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import { Suspense } from 'react';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
@@ -6,6 +7,10 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from 'next';
 import { inter } from "@/app/ui/fonts";
 
 import "@/app/ui/globals.css";
+
+export const metadata: Metadata = {
+  title: '%s | Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
