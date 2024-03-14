e0d0d97abf5dab3f4de49969a2920bba1956088a
Bence Kormos <ben.kormos@gmail.com>
2024-03-14 08:52:51 +0100

    remove timeout from revenue chart

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -26,12 +26,12 @@ export async function fetchRevenue(): Promise<Array<Revenue>> {
     // Artificially delay a response for demo purposes.
     // Don"t do this in production :)
 
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // console.log("Fetching revenue data...");
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const rows = await db.query.revenue.findMany({});
 
-    console.log("Data fetch completed after 3 seconds.");
+    // console.log("Data fetch completed after 3 seconds.");
 
     return rows;
   } catch (error) {
